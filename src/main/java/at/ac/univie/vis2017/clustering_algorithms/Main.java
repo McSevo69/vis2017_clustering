874eca8f27b160cdff99b60aa512c0a47c37277a
michaeltrimmel <a1449702@unet.univie.ac.at>
2017-12-03 15:36:14 +0100

    Read data from file

--- src/main/java/at/ac/univie/vis2017/clustering_algorithms/Main.java
+++ src/main/java/at/ac/univie/vis2017/clustering_algorithms/Main.java
@@ -1,13 +1,57 @@
 package at.ac.univie.vis2017.clustering_algorithms;
 
+import ch.netzwerg.paleo.DataFrame;
+
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
+    public Main() throws IOException {
+    }
 
-    public
+    // read data from txt-file
+    // http://people.cs.nctu.edu.tw/~rsliang/dbscan/testdatagen.html
+    public static DataFrame getDataFromTxt(String filePath, String fileName) throws IOException {
+
+        // create dataframe where txt file is saved
+        DataFrame data = null;
+        String file = filePath + fileName;
+
+
+        // create buffered reader
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
+        String line;
+
+        try {
+            while ((line = br.readLine()) != null) {
+
+                System.out.println(line + "\n");
+
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            System.out.println("ERROR: unable to read file " + file);
+            e.printStackTrace();
+        }
+
+        return data;
+    }
 
 
 
-    public static void main() {
+
+    public static void main(String[] args) throws IOException {
+
+
+        String fileName = "dbscan1.txt";
+        String filePath = System.getProperty("user.dir") + "/src/main/data/";
+
+        DataFrame dat = getDataFromTxt(filePath, fileName);
 
     }
 }
