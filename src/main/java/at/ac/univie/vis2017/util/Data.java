6c4c25008dfe0f836a77ffc221260dd2330c28d5
isaidhiptohop <richard@los-paul.eu>
2017-12-07 14:03:19 +0100

    changed data.java by adding new arraylist, also created static function to call a small test data set

--- src/main/java/at/ac/univie/vis2017/util/Data.java
+++ src/main/java/at/ac/univie/vis2017/util/Data.java
@@ -14,9 +14,10 @@ import java.util.ArrayList;
  */
 public class Data {
     private int n;
+    private int k;
     private Algorithm algorithm;
     private ArrayList<ArrayList<Point>> data;
-    
+    private ArrayList<ArrayList<Point>> centers;
     
     public Data (int n, Algorithm algorithm) {
         this.n = n;
@@ -26,14 +27,21 @@ public class Data {
     public Data (int n, Algorithm algorithm, ArrayList<Point> initData) {
         this.n = n;
         this.algorithm = algorithm;
-/*        
-        if (initData.size() < n) {
-            throw new IllegalArgumentException(
-                "the passed array list is of wrong size " + initData.size() + 
-                " (expected n=" + n + ")"
-            );
-        }
-*/        
+
+        this.data.add(initData);
+    }
+
+    public Data (int n, int k, Algorithm algorithm) {
+        this.n = n;
+        this.k = k;
+        this.algorithm = algorithm;
+    }
+    
+    public Data (int n, int k, Algorithm algorithm, ArrayList<Point> initData) {
+        this.n = n;
+        this.k = k;
+        this.algorithm = algorithm;
+
         this.data.add(initData);
     }
     
@@ -48,26 +56,89 @@ public class Data {
     }
 
     public void addIteration (ArrayList<Point> newData) {
-        this.data.add(newData);
+        data.add(newData);
     }
     
+    public void addIteration (ArrayList<Point> newData, ArrayList<Point> newCenters) {
+        data.add(newData);
+        centers.add(newCenters);
+    }
+
     public void setIteration (int iteration, ArrayList<Point> newData) {
-        this.data.set(iteration, newData);
+        data.set(iteration, newData);
     }
     
-    public ArrayList<Point> getIteration (int iteration) {
-        return this.data.get(iteration);
+    public void setIteration (int iteration, ArrayList<Point> newData, ArrayList<Point> newCenters) {
+        data.set(iteration, newData);
+        centers.set(iteration, newCenters);
+    }
+
+    public ArrayList<ArrayList<Point>> getIteration (int iteration) {
+        ArrayList<ArrayList<Point>> buf = new ArrayList<ArrayList<Point>>();
+        buf.add(data.get(iteration));
+        buf.add(centers.get(iteration));
+        return buf;
     }
     
+    public ArrayList<Point> getIterationData (int iteration) {
+        return data.get(iteration);
+    }
+
+    public ArrayList<Point> getIterationCenters (int iteration) {
+        return centers.get(iteration);
+    }
+
     public int getIterations () {
-        return this.data.size();
+        return data.size();
     }
     
     public Algorithm getAlgortihm () {
-        return this.algorithm;
+        return algorithm;
     }
     
     public int getN () {
-        return this.n;
+        return n;
+    }
+    
+    public int getK () {
+        return k;
+    }
+    
+    public static Data getTestData () {
+        int n = 5;
+        int k = 2;
+        Algorithm algorithm = Algorithm.KMEANS;
+
+        
+        Data data = new Data(n, k, algorithm);
+
+        ArrayList<Point> points = new ArrayList<Point>();
+        points.add(new Point(20, 45, 1, 1, 1));
+        points.add(new Point(22, 40, 1, 1, 1));
+        points.add(new Point(25, 43, 1, 1, 1));
+        points.add(new Point(130, 150, 1, 1, 2));
+        points.add(new Point(132, 145, 1, 1, 2));
+        
+        ArrayList<Point> centers = new ArrayList<Point>();
+        centers.add(new Point(22.3, 42.6, 1, 1, 1));
+        centers.add(new Point(131, 147.5, 1, 1, 2));
+
+        data.addIteration(points, centers);
+
+        
+        points = new ArrayList<Point>();
+        points.add(new Point(20, 45, 1, 1, 1));
+        points.add(new Point(22, 40, 1, 1, 1));
+        points.add(new Point(25, 43, 1, 1, 1));
+        points.add(new Point(130, 150, 1, 1, 2));
+        points.add(new Point(132, 145, 1, 1, 2));
+        
+        centers = new ArrayList<Point>();
+        centers.add(new Point(22.3, 42.6, 1, 1, 1));
+        centers.add(new Point(131, 147.5, 1, 1, 2));
+        
+        data.addIteration(points, centers);
+        
+        return data;
     }
 }
